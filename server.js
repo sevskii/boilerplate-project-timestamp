d1aef8a06d050f519cf4a007b16daad6172619f4
sevskii <sevskii111@gmail.com>
2017-05-18 19:37:08 +0300

    solution init

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({
+  optionSuccessStatus: 200
+})); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,10 +23,27 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({
+    greeting: 'hello API'
+  });
 });
 
-
+app.get('/api/timestamp/:date_string?', function (req, res) {
+  if (req.params.date_string.match(/^\d+$/))
+    req.params.date_string = +req.params.date_string;
+  var date = new Date(req.params.date_string);
+  if (date) {
+    res.json({
+      'unix': date.getTime(),
+      'utc': date.toUTCString()
+    });
+  } else {
+    res.json({
+      "unix": null,
+      "utc": "Invalid Date"
+    });
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
